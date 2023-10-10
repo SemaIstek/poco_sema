d60c3d275f8d4c8facd5734d403b6a12be761394
Sema <sema.istek@gmail.com>
2023-10-10 16:56:20 +0200

    Change the files

--- 4_week/script3.js
+++ 4_week/script3.js
@@ -10,18 +10,28 @@ if (number >= 0 && number <= 20) {
     console.log("You did not enter a number between 0 and 20.");
 }
 */
-function calculate() {
-    
-    var number = document.getElementById("number").value;
-    do {
-  number=Number(prompt("Please enter a number between 0 and 20."))
-} while(number < 0 || number > 20)
-    if(number>=0 && number <=20){
+function calculate() 
+{
+    let result;
+    let number = document.getElementById("number").value;
+    while(number < 0 || number > 20) {
+        number=Number(prompt("Please enter a number between 0 and 20."))
+      } 
+          
+    if(number>=0 && number <=20)
+    {
+        document.getElementById("number").value=number;
         if (number % 2 === 0) {
-        console.log("Your number is even.");
-    } else {
-        console.log("Your number is odd.");
+            result="Even";
+            console.log("Your number is even.");
+            } 
+        else {
+            result="Odd";
+            console.log("Your number is odd.");
+             }
+     
+        document.getElementById("result").innerText=result;
     }
-}
     
 }
+    
--- 4_week/script6.js
+++ 4_week/script6.js
@@ -1,4 +1,11 @@
-// @ts-check
+// @ts-nocheck
+let  output="";
+let result;
 for(let i=0; i<=10; i++){
+    result=i*9;
+    output += i + " * 9=" + result + "<br>";
     console.log(i+ " * "+ " 9 = "+ i*9);
-}
+
+}    
+    document.getElementById("multi").innerHTML=output;
+    
